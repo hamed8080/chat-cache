hamed8080/chat-extensions

Use a comment version along side with repo version to switch faster

Package.swift
@@ -17,27 +17,36 @@ let package = Package(
             targets: ["ChatExtensions"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-transceiver", from: "2.1.0"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-cache", from: "2.1.0"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-core", from: "2.1.0"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-transceiver", from: "2.1.0"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-cache", from: "2.1.0"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-core", from: "2.1.0"),
+        .package(path: "../ChatTransceiver"),
+        .package(path: "../ChatCache"),
+        .package(path: "../ChatCore"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "ChatExtensions",
             dependencies: [
-                .product(name: "ChatTransceiver", package: "chat-transceiver"),
-                .product(name: "ChatCache", package: "chat-cache"),
-                .product(name: "ChatCore", package: "chat-core"),
+//                .product(name: "ChatTransceiver", package: "chat-transceiver"),
+//                .product(name: "ChatCache", package: "chat-cache"),
+//                .product(name: "ChatCore", package: "chat-core"),
+                .product(name: "ChatTransceiver", package: "ChatTransceiver"),
+                .product(name: "ChatCache", package: "ChatCache"),
+                .product(name: "ChatCore", package: "ChatCore"),
             ]
         ),
         .testTarget(
             name: "ChatExtensionsTests",
             dependencies: [
                 "ChatExtensions",
-                .product(name: "ChatTransceiver", package: "chat-transceiver"),
-                .product(name: "ChatCache", package: "chat-cache"),
-                .product(name: "ChatCore", package: "chat-core"),
+//                .product(name: "ChatTransceiver", package: "chat-transceiver"),
+//                .product(name: "ChatCache", package: "chat-cache"),
+//                .product(name: "ChatCore", package: "chat-core"),
+                .product(name: "ChatTransceiver", package: "ChatTransceiver"),
+                .product(name: "ChatCache", package: "ChatCache"),
+                .product(name: "ChatCore", package: "ChatCore"),
             ],
             resources: []
         ),

hamed8080/chat-cache

Package.swift
@@ -17,17 +17,22 @@ let package = Package(
             targets: ["ChatCache"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-models", from: "2.1.0"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/additive", from: "1.2.2"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/mocks", from: "1.2.2"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-models", from: "2.1.0"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/additive", from: "1.2.2"),
+//        .package(url: "https://pubgi.sandpod.ir/chat/ios/mocks", from: "1.2.2"),
+        .package(path: "../ChatModels"),
+        .package(path: "../Additive"),
+        .package(path: "../Mocks"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [        
         .target(
             name: "ChatCache",
             dependencies: [
-                .product(name: "Additive", package: "additive"),
-                .product(name: "ChatModels", package: "chat-models"),
+//                .product(name: "Additive", package: "additive"),
+//                .product(name: "ChatModels", package: "chat-models"),
+                .product(name: "Additive", package: "Additive"),
+                .product(name: "ChatModels", package: "ChatModels"),
             ],
             resources: [.process("Resources")]
         ),
@@ -35,9 +40,12 @@ let package = Package(
             name: "ChatCacheTests",
             dependencies: [
                 "ChatCache",
-                .product(name: "Additive", package: "additive"),
-                .product(name: "ChatModels", package: "chat-models"),
-                .product(name: "Mocks", package: "mocks"),
+//                .product(name: "Additive", package: "additive"),
+//                .product(name: "ChatModels", package: "chat-models"),
+//                .product(name: "Mocks", package: "mocks"),
+                .product(name: "Additive", package: "Additive"),
+                .product(name: "ChatModels", package: "ChatModels"),
+                .product(name: "Mocks", package: "Mocks"),
             ]
         ),
     ]